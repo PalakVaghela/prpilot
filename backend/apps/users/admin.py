a6a9415cf12133af887bd9a671cc6b3a82f2fdd7
PalakVaghela <palakvaghela83@gmail.com>
2026-07-13 15:59:49 +0530

    [IMP] Created a user table and admin dashboard for the same

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+admin.site.register(User)
+# because django provide a admin panel so if we register our model in it, data will visible there without any
--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    github_id = models.CharField(max_length=100, unique=True, null=True, blank=True)
+    github_username = models.CharField(max_length=250, unique=True, null=True, blank=True)
+    avatar_url = models.URLField(blank=True)
+    profile_url = models.URLField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
